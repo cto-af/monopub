d1f4aa3da6a61de05df9afa4fb2269d86f19e1f8
Joe Hildebrand <joe-github@cursive.net>
2024-10-25 11:12:19 -0600

    Initial checkin

---
+++ README.md
@@ -0,0 +1,22 @@
+# @cto.af/monopub
+
+Description
+
+## Installation
+
+```sh
+npm install @cto.af/monopub
+```
+
+## API
+
+Full [API documentation](http://cto-af.github.io/monopub/) is available.
+
+Example:
+
+```js
+```
+
+---
+[![Build Status](https://github.com/cto-af/monopub/workflows/Tests/badge.svg)](https://github.com/cto-af/monopub/actions?query=workflow%3ATests)
+[![codecov](https://codecov.io/gh/cto-af/monopub/branch/main/graph/badge.svg?token=N7B7YLIDM4)](https://codecov.io/gh/cto-af/monopub)
---
+++ typedoc.config.cjs
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('typedoc').TypeDocOptions} */
+module.exports = {
+  entryPoints: ['src/index.ts'],
+  out: 'docs',
+  cleanOutputDir: true,
+  sidebarLinks: {
+    GitHub: 'https://github.com/hildjj/cto-af/monopub/',
+    Documentation: 'http://cto-af.github.io/monopub/',
+  },
+  navigation: {
+    includeCategories: false,
+    includeGroups: false,
+  },
+  includeVersion: true,
+  categorizeByGroup: false,
+  sort: ['static-first', 'alphabetical'],
+  exclude: ['**/*.spec.ts'],
+};
